913a59e7a7a7bd9b0a48a14933169694a4af44e6
Andrelcs12 <126403873+Andrelcs12@users.noreply.github.com>
2024-11-19 12:14:02 -0300

    Delete landing page/02 directory

--- landing page/02/script.js
+++
@@ -1,8 +0,0 @@
-/*https://nicepage.com/website-design/preview/family-friendly-pizza-restaurant-4694183?device=desktop */
-
-const btn = document.querySelector('.btn');
-const login = document.querySelector('.loginPage');
-
-btn.onclick = () => {
-    login.classlist.add('active');
-}
